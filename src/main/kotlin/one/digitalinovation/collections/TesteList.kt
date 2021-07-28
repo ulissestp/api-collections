448a1dd4129621e7803632cda5a96b0ef90147ce
ulissestp <ulisses.pierine@gmail.com>
2021-07-27 22:25:12 -0300

    Organizando a lista com grupos

--- src/main/kotlin/one/digitalinovation/collections/TesteList.kt
+++ src/main/kotlin/one/digitalinovation/collections/TesteList.kt
@@ -1,9 +1,9 @@
 package one.digitalinovation.collections
 
 fun main() {
-    val joao = Funcionario(nome = "João", salario = 2000.0)
-    val pedro = Funcionario(nome = "Pedro", salario = 1500.0)
-    val maria = Funcionario(nome = "Maria", salario = 4000.0)
+    val joao = Funcionario(nome = "João", salario = 2000.0, tipoContratacao = "CLT")
+    val pedro = Funcionario(nome = "Pedro", salario = 1500.0, tipoContratacao = "PJ")
+    val maria = Funcionario(nome = "Maria", salario = 4000.0, tipoContratacao = "CLT")
 
     val funcionarios = listOf(joao, pedro, maria)
 
@@ -15,11 +15,19 @@ fun main() {
     funcionarios
         .sortedBy { it.salario }
         .forEach { println(it) }
+
+    println("----agrupar por tipo de contratacao------")
+    funcionarios
+        .groupBy { it.tipoContratacao }
+        .forEach { println(it) }
+
+
 }
 
 data class Funcionario(
     val nome: String,
-    val salario: Double
+    val salario: Double,
+    val tipoContratacao: String
 ){
     override fun toString(): String =
         """
